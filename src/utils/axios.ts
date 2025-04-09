6a2e0495d0372dbfe2941107a041acb33d1e76c1
Zhangluo <643733581@qq.com>
2025-04-09 16:39:12 +0800

    :bug: fix: 修复请求头的bug

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // 创建自定义实例
 const http = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || '/api', // 从环境变量获取
+  baseURL: import.meta.env.VITE_API_BASE_URL || '', // 从环境变量获取
   timeout: 10000, // 请求超时时间
   headers: {
     'Content-Type': 'application/json;charset=UTF-8'
